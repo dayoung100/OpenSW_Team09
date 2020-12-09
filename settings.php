ab4ffa2b11da4a48c3c6998b2fabff81c382b656
Taehee-K <63901494+Taehee-K@users.noreply.github.com>
2020-12-10 02:52:16 +0900

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,19 +2,50 @@
     require "header.php"
 ?> 
 
-<main>
-            <section id = "main_section">
-                <div class="signup">
-                    <h1>Signup</h1>
-                    <form action = "includes/signup.inc.php" method = "post">
-                        <p><input type = "text" name = "uid" placeholder = "Username">
-                        <p><input type = "text" name = "mail" placeholder = "E-mail">
-                        <p><input type = "password" name = "pwd" placeholder = "Password">
-                        <p><input type = "password" name = "pwd-repeat" placeholder = "Repeat Password">
-                        <p><button type = "submit" name = "signup-submit">Signup</button>
-                    </form>
-                </signup>
-            </section>
+        <main>
+        <section id= "main_section">
+            <section id="settings">
+            <div class = "account-info">
+                <h1 style="text-align: center;">Account Information</h1>
+                <form action = "includes/editinfo.inc.php" method="post">
+
+                <p>Username
+                <input type="text" placeholder="Enter Username" name="uid" id="uid" required>
+
+                <p>Email
+                <input type="text" placeholder="Enter Email" name="mail" id="mail" required>
+
+                <p>Password
+                <input type="password" placeholder="Enter Password" name="pwd" id="pwd" required>
+
+                <p>Confirm Changed Password
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+
+                <p><button type="submit" name="change-info">Change My Info</button>
+                </form>
+            </div>  
+            <p>
+            <div class="pet-info">
+                <h1 style="text-align: center;">Pet Information</h1>
+                <form action = "includes/petinfo.inc.php" method="post">
+
+                <p>Name
+                <input type="text" placeholder="Enter Username" name="uid" id="uid" required>
+
+                <p>Type
+                <input type="text" placeholder="Enter Email" name="mail" id="mail" required>
+
+                <p>Breed
+                <input type="password" placeholder="Enter Password" name="pwd" id="pwd" required>
+
+                <p>Birthday
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+
+                <p><button type="submit" name="pet-info">Add Pet</button>
+                </form>
+            </div>
+        </section>
+        </section>
         </main>
         <footer id="main_footer">
             Copyright &copy;2020 by Taehee Kim, Seoyeon Ahn, Dayoung Lee, Seoyeong Han
