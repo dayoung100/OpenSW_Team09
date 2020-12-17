2a86c1b85622e1fb8a9906b50985f83d93025058
Taehee-K <63901494+Taehee-K@users.noreply.github.com>
2020-12-17 09:45:22 +0900

    Add files via upload

---
+++ test/intro1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <class style = "text-align: center; font-family: Arial, Helvetica, sans-serif; ">
+    <h1 >Pet-Book</h1>
+    <figure>
+        <img src=pet.jpg style="margin-left:10%">
+    </figure>
+    <h3>Record and Manage Your Pet's Schedule!</h3>
+    </class>
+</body>
+
+</html>
---
+++ test/logout.php
@@ -0,0 +1,10 @@
+
+<?php
+    require "header.php"
+?> 
+            
+        <main>  
+            <section id = "main_section">
+                <iframe src ="logout.html" height ="800" width = "100%"></iframe>
+            </section>
+        </main>
