5a3a94ee854066cfb27cf7b2ead694cab9d306ec
Taehee-K <63901494+Taehee-K@users.noreply.github.com>
2020-12-17 10:42:53 +0900

    Add files via upload

---
+++ test/intro2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class style = "margin-left: 5%;text-align: left; font-family: Arial, Helvetica, sans-serif; ">
+    <section style = "width: 350px; border:none; padding: 10px; background-color: #effad3;">
+    <h2>1. Record daily To-Do list</h2>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Keep track of important days!
+    </section>
+<br>
+<br>
+    <section style = "width: 350px; border:none; padding: 10px; background-color: #effad3;">
+    <h2>2. Track your spendings</h2>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Manage your pet-budget!
+    </section>
+<br>
+<br>
+    <section style = "width: 350px; border:none; padding:10px; background-color: #effad3;">
+    <h2>3. Log your stories</h2>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A diary of memorable days with your pet!
+    </section>
+
+    </class>
+</body>
+
+</html>
